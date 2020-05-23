833a32d3e43e61d441963bbb1952038df4bf784c
Xiaokang Guo <xg590@nyu.edu>
2020-05-22 22:06:28 -0400

    Add files via upload

---
+++ autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "dbname",
+  "dbuser"        => "username",
+  "dbpass"        => "passwd",
+  "dbhost"        => "db_owncloud:3306",
+  "dbtableprefix" => "",
+  "adminlogin"    => "admin_name",
+  "adminpass"     => "admin_passwd",
+  "directory"     => "/var/www/html/nextcloud/data",
+); 
